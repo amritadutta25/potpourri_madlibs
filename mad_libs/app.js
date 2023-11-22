ada0cf619a1947ef5c08fdba306972363fb16597
Amrita Dutta <amrita.dutta1497@gmail.com>
2023-11-22 09:55:42 -0500

    Added logic to ask the user if they want to start the game again or not

--- mad_libs/app.js
+++ mad_libs/app.js
@@ -31,3 +31,22 @@ function startMadlib(words)
 
 startMadlib(words)
 // console.log(words)
+
+
+// to start the game again
+const startResponse = prompt(`Do you want to start again? Enter y/n:`)
+
+if (startResponse === 'y')
+{   
+    startMadlib(words)
+}
+if (startResponse === 'n')
+{
+    alert("End of game")
+}
+if (response !== 'y' && response !=='n')
+{
+    alert("Invalid reponse, please enter 'y' or 'n'")
+}
+
+// Ques - this code starts the game only once, how can I keep on asking the user if they want to start again until they press 'n' to stp the game?
